162aaad36599d05a766270775e6d4a86a1a0ce18
MunjN <134438716+MunjN@users.noreply.github.com>
2025-12-02 14:05:00 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// For local dev, base="/" is perfect.
-// Later for GitHub Pages we'll change base to "/<your-repo-name>/"
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base: "/AI-Dashboard-ReAct/"
 });
